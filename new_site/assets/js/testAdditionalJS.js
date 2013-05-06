550c28d052b09189fba1e23866da68a221f8f74c
Bo Lora <bolora@hotmail.com>
2013-05-06 11:52:21 -0500

    Clean up master for starter kit

--- new_site/assets/js/testAdditionalJS.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Jerry_Orta
- * Date: 4/12/13
- * Time: 2:49 PM
- * To change this template use File | Settings | File Templates.
- */
-
-console.log("additional js loaded:");
