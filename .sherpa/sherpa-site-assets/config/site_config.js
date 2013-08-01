39f8bfbddf4080907729fea38764df6e3bdc94c2
Bo Lora <bolora@hotmail.com>
2013-08-01 12:50:40 -0500

    added a dell-308-232 theme to use bootstrap 232 css

--- .sherpa/sherpa-site-assets/config/site_config.js
+++ .sherpa/sherpa-site-assets/config/site_config.js
@@ -33,6 +33,7 @@ SHERPA.JS_LIB_PATH = ".sherpa/lib/";
 SHERPA.CORE_PATH = ".sherpa/";
 SHERPA.COMPONENTS_PATH = ".sherpa/components/";
 SHERPA.ASSETS_PATH = ".sherpa/sherpa-site-assets/";
+SHERPA.DEFAULT_THEME = "dell-308-232";
 
 // i18n Configuration
 SHERPA.DEFAULT_LOCALE = "en";
@@ -99,8 +100,7 @@ SHERPA.PROTOTYPE_APP = "sherpa-site.js";
 SHERPA.PROTOTYPE_CSS = "sherpa-site-styles.css";
 
 SHERPA.CSS_CORE_LOAD = [
-	{ bootstrap: SHERPA.CSS_CORE_PATH+"bootstrap.css" },
-	{ css_theme: SHERPA.CSS_CORE_PATH+"sherpa-override.css" },
+	{ css_theme: SHERPA.CSS_CORE_PATH+SHERPA.DEFAULT_THEME+".css" },
 	{ sherpa_uxmf: SHERPA.CSS_CORE_PATH+"sherpa-uxmf.css" }
 ];
 SHERPA.CSS_GRID = SHERPA.CSS_CORE_PATH+"grid/gridset.css";
