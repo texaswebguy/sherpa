4f60b79f38cb804201b75fcf2769e5a21f993a26
Bo Lora <bolora@hotmail.com>
2013-10-14 10:50:47 -0500

    added active state to masthead link

--- _experimental/masthead/assets/js/masthead.js
+++ _experimental/masthead/assets/js/masthead.js
@@ -1,13 +1,15 @@
 
 // Controllers 
 
-sherpaApp.controller("mastheadController", function($scope) {
+sherpaApp.controller("mastheadController", function($scope,$state) {
     
 	$scope.routes = SHERPA.PROTO_ROUTES;
+	$scope.$state = $state;
+	console.log($state)
 	
 });
-sherpaApp.controller("footerController", function($scope) {
+sherpaApp.controller("footerController", function($scope,$state) {
     
 	$scope.routes = SHERPA.PROTO_ROUTES;
-	
+	$scope.$state = $state;
 });
