89ec3918617085ef33ffa10b5e55c640ab9386db
Bo Lora <bolora@hotmail.com>
2013-10-14 10:38:34 -0500

    added sample footer with routing to _experimental/masthead

--- _experimental/masthead/assets/js/masthead.js
+++ _experimental/masthead/assets/js/masthead.js
@@ -6,3 +6,8 @@ sherpaApp.controller("mastheadController", function($scope) {
 	$scope.routes = SHERPA.PROTO_ROUTES;
 	
 });
+sherpaApp.controller("footerController", function($scope) {
+    
+	$scope.routes = SHERPA.PROTO_ROUTES;
+	
+});
