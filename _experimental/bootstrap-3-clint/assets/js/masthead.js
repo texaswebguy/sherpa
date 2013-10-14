828ceb64ca1e3abbe06c0d10497e24f5e009e938
clintbatte <clint_batte@dell.com>
2013-10-14 11:20:50 -0500

    new file:   _experimental/bootstrap-3-clint/assets/config/masthead.json 	new file:   _experimental/bootstrap-3-clint/assets/config/project_info.csv 	new file:   _experimental/bootstrap-3-clint/assets/config/settings_cn.json 	new file:   _experimental/bootstrap-3-clint/assets/config/settings_en.json 	new file:   _experimental/bootstrap-3-clint/assets/content/messages_cn.json 	new file:   _experimental/bootstrap-3-clint/assets/content/messages_en.json 	new file:   _experimental/bootstrap-3-clint/assets/css/masthead.css 	new file:   _experimental/bootstrap-3-clint/assets/data/sample_customer_list_en.csv 	new file:   _experimental/bootstrap-3-clint/assets/img/sample-banner-image.png 	new file:   _experimental/bootstrap-3-clint/assets/includes/footer.html 	new file:   _experimental/bootstrap-3-clint/assets/includes/masthead.html 	new file:   _experimental/bootstrap-3-clint/assets/js/masthead.js 	new file:   _experimental/bootstrap-3-clint/assets/xref/resources_cn.json 	new file:   _experimental/bootstrap-3-clint/assets/xref/resources_en.json 	new file:   _experimental/bootstrap-3-clint/index.html 	new file:   _experimental/bootstrap-3-clint/pages/page1.html 	new file:   _experimental/bootstrap-3-clint/pages/page2.html 	new file:   _experimental/bootstrap-3-clint/pages/page3.html 	modified:   _sherpa/config/core-config.json 	deleted:    _sherpa/css-source/less-BS3/accordion.less 	modified:   _sherpa/css-source/less-BS3/alerts.less 	modified:   _sherpa/css-source/less-BS3/badges.less 	modified:   _sherpa/css-source/less-BS3/bootstrap.less 	modified:   _sherpa/css-source/less-BS3/button-groups.less 	modified:   _sherpa/css-source/less-BS3/buttons.less 	modified:   _sherpa/css-source/less-BS3/carousel.less 	modified:   _sherpa/css-source/less-BS3/close.less 	modified:   _sherpa/css-source/less-BS3/code.less 	modified:   _sherpa/css-source/less-BS3/dropdowns.less 	modified:   _sherpa/css-source/less-BS3/forms.less 	new file:   _sherpa/css-source/less-BS3/glyphicons.less 	modified:   _sherpa/css-source/less-BS3/grid.less 	new file:   _sherpa/css-source/less-BS3/input-groups.less 	modified:   _sherpa/css-source/less-BS3/jumbotron.less 	modified:   _sherpa/css-source/less-BS3/labels.less 	modified:   _sherpa/css-source/less-BS3/list-group.less 	modified:   _sherpa/css-source/less-BS3/mixins.less 	modified:   _sherpa/css-source/less-BS3/modals.less 	modified:   _sherpa/css-source/less-BS3/navbar.less 	modified:   _sherpa/css-source/less-BS3/navs.less 	modified:   _sherpa/css-source/less-BS3/pager.less 	modified:   _sherpa/css-source/less-BS3/pagination.less 	modified:   _sherpa/css-source/less-BS3/panels.less 	modified:   _sherpa/css-source/less-BS3/popovers.less 	modified:   _sherpa/css-source/less-BS3/progress-bars.less 	modified:   _sherpa/css-source/less-BS3/responsive-utilities.less 	modified:   _sherpa/css-source/less-BS3/scaffolding.less 	modified:   _sherpa/css-source/less-BS3/tables.less 	new file:   _sherpa/css-source/less-BS3/theme.less 	modified:   _sherpa/css-source/less-BS3/thumbnails.less 	modified:   _sherpa/css-source/less-BS3/tooltip.less 	modified:   _sherpa/css-source/less-BS3/type.less 	modified:   _sherpa/css-source/less-BS3/utilities.less 	modified:   _sherpa/css-source/less-BS3/variables.less 	modified:   _sherpa/css-source/less-BS3/wells.less 	new file:   _sherpa/css/themes/dell-308-theme-v0.8

---
+++ _experimental/bootstrap-3-clint/assets/js/masthead.js
@@ -0,0 +1,13 @@
+
+// Controllers 
+
+sherpaApp.controller("mastheadController", function($scope) {
+    
+	$scope.routes = SHERPA.PROTO_ROUTES;
+	
+});
+sherpaApp.controller("footerController", function($scope) {
+    
+	$scope.routes = SHERPA.PROTO_ROUTES;
+	
+});
