dc6551ac897fe43adaa444e324cf15efc40071c5
Jerry Orta <jerryorta@gmail.com>
2013-10-30 13:38:11 -0500

    $watch test01

---
+++ _experimental/watcher_non_sherpa/test01/app.js
@@ -0,0 +1,43 @@
+var test = [{value: 1},
+    {value: 2},
+    {value: 3},
+    {value: 4},
+    {value: 5},
+    {value: 6}];
+
+var testObj = [{value: 1}];
+
+var app = angular.module('watchApp', []);
+app.controller('watchCtrl', function($scope) {
+    $scope.count = 0;
+    $scope.b = test;
+
+    $scope.$watch('b', function() {
+        // do something here
+        $scope.count += 1;
+    }, true);
+
+    $scope.objectValue = testObj;
+
+    $scope.$watch('objectValue', function() {
+        // do something here
+        $scope.count += 1;
+    }, true);
+});
+
+app.controller('watchCtrl2', function($scope) {
+    $scope.count = 0;
+    $scope.b = test;
+
+    $scope.$watch('b', function() {
+        // do something here
+        $scope.count += 1;
+    }, true);
+
+    $scope.objectValue = testObj;
+
+    $scope.$watch('objectValue', function() {
+        // do something here
+        $scope.count += 1;
+    }, true);
+});
