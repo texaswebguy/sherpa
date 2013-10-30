c69042860ba68582a7d0c29cbb0e0a8a739b69c3
Jerry Orta <jerryorta@gmail.com>
2013-10-30 14:44:50 -0500

    test 03

--- _experimental/watcher_non_sherpa/test03/app.js
+++ _experimental/watcher_non_sherpa/test03/app.js
@@ -5,7 +5,45 @@ var test = [{value: 1},
     {value: 5},
     {value: 6}];
 
-var obj = {value: 1};
+/**
+ * Create a global object with functionality using the observer software pattern.
+ * @constructor
+ */
+function ObserverClass() {
+    this.value = 2000;
+    this.observers = [] //Contains functions to
+}
+
+ObserverClass.prototype = {
+
+    constructor: ObserverClass,
+
+    subscribe: function ( callbackFunction ) {
+        this.observers.push( callbackFunction );
+    },
+
+    notify: function() {
+      var len = this.observers.length;
+      for ( var i = 0; i < len; i++ ) {
+          this.observers[i].apply( null, null );
+      }
+    },
+
+    setValue:function ( newValue ) {
+        this.value = newValue;
+        this.notify();
+        return this.value; //so you don't return "undefined" in the console.
+    }
+
+}
+
+/**
+ *  Instantiate the ObserverClass and assign it to a global object.
+ * @type {ObserverClass}
+ */
+var globalObject = new ObserverClass();
+
+
 
 var app = angular.module('watchApp', []);
 app.controller('watchCtrl', function($scope) {
@@ -17,12 +55,22 @@ app.controller('watchCtrl', function($scope) {
         $scope.count += 1;
     }, true);
 
-    $scope.objectValue = obj;
+    $scope.objectValue = globalObject;
 
     $scope.$watch('objectValue', function() {
         // do something here
         $scope.count += 1;
     }, true);
+
+    function callBack() {
+        console.log( "callback from watchCtrl");
+
+        //$digest makes $watch to (dirty) check if values are updated.
+        $scope.$digest();
+    }
+
+    globalObject.subscribe( callBack );
+
 });
 
 app.controller('watchCtrl2', function($scope) {
@@ -34,10 +82,23 @@ app.controller('watchCtrl2', function($scope) {
         $scope.count += 1;
     }, true);
 
-    $scope.objectValue = obj;
+    $scope.objectValue = globalObject;
 
     $scope.$watch('objectValue', function() {
         // do something here
         $scope.count += 1;
     }, true);
+
+
+
+    function callBack() {
+        console.log( "callback from watchCtrl2");
+
+        //$digest makes $watch to (dirty) check if values are updated.
+        $scope.$digest();
+    }
+
+    globalObject.subscribe( callBack );
 });
+
+console.log( "To test, enter 'globalObject.setValue( [some value] )'.")
