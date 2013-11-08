01a2f383da2343f7500041d2131ce2981d51da66
clintbatte <clint_batte@dell.com>
2013-11-08 16:31:28 -0600

    modified:   _experimental/bootstrap-3-clint/assets/js/app.js 	modified:   _experimental/bootstrap-3-clint/index.html 	modified:   _sherpa/css-source/themes/dell-308-bs3/overrides.less 	modified:   _sherpa/css/themes/dell-308-theme-v0.8.css

--- _experimental/bootstrap-3-clint/assets/js/app.js
+++ _experimental/bootstrap-3-clint/assets/js/app.js
@@ -1,3 +1,40 @@
+  $('#carousel-example-gallery').carousel({
+  interval: 10000
+  });
+  $('#carousel-example-gallery-caption').carousel({
+  interval: 10000
+  });
 
-// Controllers 
+  $('[data-toggle=tooltip]').tooltip();
 
+  $('[data-toggle="popover"]').popover({trigger:'manual'});
+$('[data-toggle="popover"]').click(function(event){
+  event.preventDefault();
+  $('[data-toggle="popover"]').popover('destroy');
+  $(this).popover('show');
+  $('[data-dismiss="popover"]').click(function(event){
+    event.preventDefault();
+    $(this).parents('.popover').prev().popover('hide');
+  });
+});
+$('[data-toggle="popover"][data-trigger="hover"]').mouseover(function(event){
+  event.preventDefault();
+  $('[data-toggle="popover"]').popover('destroy');
+  $(this).popover('show');
+});
+$('[data-toggle="popover"][data-trigger="hover"]').mouseout(function(event){
+  $(this).popover('destroy');
+});
+$('[data-toggle="popover"][data-trigger="hover"]').focus(function(event){
+  $('[data-toggle="popover"]').popover('destroy');
+  $(this).popover('show');
+});
+
+$('#custom-popover').click(function(event){
+  event.preventDefault();
+  $(this).popover('show');
+  $('[data-dismiss="popover"]').click(function(event){
+    event.preventDefault();
+    $(this).parents('.popover').prev().popover('hide');
+  });
+});
